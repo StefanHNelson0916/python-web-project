066fb805622af9f57c213e82c151e64824f7f332
StefanHNelson0916 <stefan.h.nelson@outlook.com>
2022-03-16 15:49:56 -0500

    added login and  register route

--- flaskDemo/routes.py
+++ flaskDemo/routes.py
@@ -1,15 +1,54 @@
 import os
 import secrets
+import yaml
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskDemo import app
+from flaskDemo import app, db, bcrypt, mysql
+from flaskDemo.forms import LoginForm, RegistrationForm
+from flaskDemo.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+from datetime import datetime
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, user_type=form.userType.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/test/')
 def test():

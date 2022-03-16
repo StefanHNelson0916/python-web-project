4ba0aa760d292cfd053c47da0a0b67eee28872b7
StefanHNelson0916 <stefan.h.nelson@outlook.com>
2022-03-16 15:41:08 -0500

    added imports, database connection

--- flaskDemo/__init__.py
+++ flaskDemo/__init__.py
@@ -1,9 +1,28 @@
 from flask import Flask
-from logging import FileHandler,WARNING
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import yaml
 
 app = Flask(__name__)
+mysql = MySQL(app)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:EchteLiebe0711!@localhost:3306/pythonwebproject'
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+db = SQLAlchemy(app)
 
-file_handler = FileHandler('errorlog.txt')
-file_handler.setLevel(WARNING)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskDemo import routes
+from flaskDemo import models
+
+models.db.create_all()

f1c7861447308681c70ccd527abedfd0ee1ce1b6
StefanHNelson0916 <stefan.h.nelson@outlook.com>
2022-03-15 19:35:36 -0500

    added error loggging

--- flaskDemo/__init__.py
+++ flaskDemo/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from logging import FileHandler,WARNING
 
 app = Flask(__name__)
+
+file_handler = FileHandler('errorlog.txt')
+file_handler.setLevel(WARNING)
 
 from flaskDemo import routes

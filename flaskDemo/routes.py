e8acc4c1d6fd03ba83eb502860be56e5e8d51302
StefanHNelson0916 <stefan.h.nelson@outlook.com>
2022-03-23 14:08:07 -0500

    added routes for new templates

--- flaskDemo/routes.py
+++ flaskDemo/routes.py
@@ -1,11 +1,12 @@
 import os
 import secrets
 import yaml
+from flask_mysqldb import MySQL
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskDemo import app, db, bcrypt, mysql
-from flaskDemo.forms import LoginForm, RegistrationForm
-from flaskDemo.models import User, Post, Project
+from flaskDemo import app, db, bcrypt
+from flaskDemo.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, CreateProjectForm
+from flaskDemo.models import User, Post, Bid, Contractor, Contractor_Skills, Customer, Project, Skills, Supplied, Supplier
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime
 
@@ -55,10 +56,101 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/customer_home')
-def customer_home():
-    results = Project.query.all()
-    return render_template('customer_home.html', results = results)
+#save picture route
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+#Route to users acccunt
+#Renders account.html
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
+
+#Route for the customer to create a project
+#Uses CreateProjectForm from form.py
+#Renders create_project.html
+@app.route("/create/project", methods=['GET', 'POST'])
+@login_required
+def create_project():
+    user_type = current_user.user_type
+    if user_type == 'Customer':
+        form = CreateProjectForm()
+        if form.validate_on_submit():
+            count = Project.query.count()
+            project = Project(projectID = count + 1 ,customerID = current_user.id, projDesc = form.projDesc.data, startDate = form.startDate.data, endDate = form.endDate.data)
+            db.session.add(project)
+            db.session.commit()
+            flash('You have created a new project!','success')
+            return redirect(url_for('home'))
+        return render_template('create_project.html', title='Create Project', form=form, legend='Create Project')
+
+    else:
+        return render_template('unauthorized.html')
+
+#Route for the customer to view their projects
+#SQLAlchemy query to find all projects associated with current_user.id
+#Renders customer_projects.html
+@app.route("/Customer/Projects")
+@login_required
+def my_projects():
+    if current_user.user_type == 'Customer':
+        results = Project.query.filter_by(current_user.id)
+        return render_template('', results=results)
+    else:
+        return render_template('unauthorized.html')
+
+#Route for the Customer to see any bids that have been placed on their projects
+#SQLAlchemy query to find all the bids on projects that current_user has ongoing
+#Renders my_bids.html 
+@app.route("/CustomersBids")
+@login_required
+def CustomersBids():
+    if current_user.user_type == 'Customer':
+        results = Project.query.filter_by(customerID =current_user.id) \
+            .join(Bid,Project.projectID==Bid.projectID) \
+                .join(Contractor,Project.contractorID==Contractor.contractorID) \
+                    .add_columns(Bid.projectID,Bid.contractorID,Bid.priceDesc,Bid.price,Bid.hours, Contractor.name)
+        return render_template('my_bids.html', joined_m_n=results)
+    else:
+        return render_template('unauthorized.html')
+
+#Route for the contractors to view their projects
+#SQLAlchemy query to find all projects associated with current_user.id
+#Renders customer_projects.html
+@app.route("/Contractor/Projects")
+@login_required
+def contractors_projects():
+    if current_user.user_type == 'Contractor':
+        results = Project.query.filter_by(current_user.id)
+        return render_template('', results=results)
+    else:
+        return render_template('unauthorized.html')
 
 @app.route('/test/')
 def test():

d47a791a6f8a88c5d9e90f1cd7e1f996cd82b77c
StefanHNelson0916 <stefan.h.nelson@outlook.com>
2022-03-29 14:05:16 -0500

    added missing routes

--- flaskDemo/routes.py
+++ flaskDemo/routes.py
@@ -4,7 +4,7 @@ import yaml
 from flask_mysqldb import MySQL
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskDemo import app, db, bcrypt
+from flaskDemo import app, db, bcrypt, mysql
 from flaskDemo.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, CreateProjectForm
 from flaskDemo.models import User, Post, Bid, Contractor, Contractor_Skills, Customer, Project, Skills, Supplied, Supplier
 from flask_login import login_user, current_user, logout_user, login_required
@@ -116,7 +116,7 @@ def create_project():
 #Route for the customer to view their projects
 #SQLAlchemy query to find all projects associated with current_user.id
 #Renders customer_projects.html
-@app.route("/Customer/Projects")
+@app.route("/CustomersProjects")
 @login_required
 def my_projects():
     if current_user.user_type == 'Customer':
@@ -127,31 +127,97 @@ def my_projects():
 
 #Route for the Customer to see any bids that have been placed on their projects
 #SQLAlchemy query to find all the bids on projects that current_user has ongoing
-#Renders my_bids.html 
+#Renders customers_bids.html 
 @app.route("/CustomersBids")
 @login_required
-def CustomersBids():
+def customers_bids():
     if current_user.user_type == 'Customer':
         results = Project.query.filter_by(customerID =current_user.id) \
             .join(Bid,Project.projectID==Bid.projectID) \
                 .join(Contractor,Project.contractorID==Contractor.contractorID) \
                     .add_columns(Bid.projectID,Bid.contractorID,Bid.priceDesc,Bid.price,Bid.hours, Contractor.name)
-        return render_template('my_bids.html', joined_m_n=results)
+        return render_template('customers_bids.html', joined_m_n=results)
     else:
         return render_template('unauthorized.html')
 
-#Route for the contractors to view their projects
-#SQLAlchemy query to find all projects associated with current_user.id
-#Renders customer_projects.html
-@app.route("/Contractor/Projects")
+#Route for the contractor too see all the skills that are associated with their ID
+#Renders contractors_skills_home.html
+@app.route("/ContractorSkills")
 @login_required
-def contractors_projects():
+def contractors_skills():
     if current_user.user_type == 'Contractor':
-        results = Project.query.filter_by(current_user.id)
-        return render_template('', results=results)
-    else:
-        return render_template('unauthorized.html')
+        results = Contractor_Skills.query.filter_by(contractorID = current_user.id) \
+            .join(Skills,Contractor_Skills.skillID == Skills.skillID) \
+                .add_columns(Skills.skillID,Skills.skillName,Skills.description,Contractor_Skills.yearsExperience,Contractor_Skills.certification,Contractor_Skills.contractorID)
+        return render_template('contractor_skills.html', joined_m_n=results)
 
-@app.route('/test/')
-def test():
-    return render_template('test.html')
+#Route to delete the selected skill from the contractors account
+@app.route("/ContractorSkills/<contractorID>/<skillID>delete", methods=['POST'])
+@login_required
+def delete_skill(contractorID,skillID):
+    contractor_skill = Contractor_Skills.query.get_or_404([contractorID,skillID])
+    db.session.delete(contractor_skill)
+    db.session.commit()
+    flash('Your Skill has been deleted!', 'success')
+    return redirect(url_for('contractors_skills'))
+
+#Route to update the selected skill from the contractors account
+#Uses form UpdateSkillForm
+#Renders update_skill.html
+@app.route("/ContractorSkills/<contractorID>/<skillID>update", methods=['GET','POST'])
+@login_required
+def update_skill(contractorID, skillID):
+    form = UpdateSkillForm()
+    contractor_skill = Contractor_Skills.query.get_or_404([contractorID, skillID])
+    if form.validate_on_submit():
+        contractor_skill.skillID = form.skillID.data
+        contractor_skill.yearsExperience = form.yearsExperience.data
+        contractor_skill.certification = form.certification.data
+        db.session.commit()
+        flash('Your Skill has been updated!', 'success')
+        return redirect(url_for('contractors_skills'))
+    return render_template('update_skill.html', title='Update Skill', form=form, contractor_skill=contractor_skill)
+
+@app.route("/AllBids")
+@login_required
+def all_bids():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM project WHERE contractorID IN (SELECT contractorID FROM bid)")
+    if resultValue > 0:
+        bidResults = cur.fetchall()
+        return render_template('all_bids.html', bidResults=bidResults)
+
+@app.route("/SkillsOffered")
+@login_required
+def skills_offered():
+    cur = mysql.connection.cursor()
+    resultsValue = cur.execute("SELECT name, address, description, hourlyRate, phoneNumber, skillID, yearsExperience, certification FROM contractor LEFT JOIN contractor_skills ON contractor.contractorID = contractor_skills.contractorID")
+    if resultsValue > 0:
+        skillsResults = cur.fetchall()
+        return render_template('skills_offered.html', skillsResults=skillsResults)
+
+@app.route("/SuppliedInformation")
+@login_required
+def supplied_information():
+    cur = mysql.connection.cursor()
+    resultsValue = cur.execute("SELECT MIN(supplyQty) as minSupplyQty, MAX(supplyQty) as maxSupplyQty, AVG(supplyQty) as avgSupplyQty, MIN(supplyPrice) as minSupplyPrice, MAX(supplyPrice) as maxSupplyPrice, AVG(supplyPrice) as avgSupplyPrice FROM supplied; ")
+    if resultsValue > 0:
+        suppliedInfoResults = cur.fetchall()
+        return render_template('supplied_info.html', suppliedInfoResults=suppliedInfoResults)
+
+@app.route("/SuppliedInformation2")
+@login_required
+def supplied_information2():
+    cur = mysql.connection.cursor()
+    resultsValue = cur.execute("SELECT * FROM supplied WHERE supplyQty >=5 AND supplyPrice < 10")
+    if resultsValue > 0:
+        suppliedInfoResults2 = cur.fetchall()
+        return render_template('supplied_info2.html', suppliedInfoResults2=suppliedInfoResults2)
+
+@app.route("/ActiveProject")
+@login_required
+def active_projects():
+    results = Project.query.filter_by(contractorID = current_user.id) \
+        .filter_by(projStatus = 'Not Done')
+    return render_template('active_projects.html',results=results)
+
